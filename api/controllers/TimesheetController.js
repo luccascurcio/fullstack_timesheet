44bb29549255acdbcd1fb3a1ddf30c04c7d6b081
unknown <luccascurcio@gmail.com>
2022-02-03 16:36:35 -0500

    Fix: editing time in mobile, when time does not carry seconds in the variable. Fix: timesheet validation consider multiples users.

--- api/controllers/TimesheetController.js
+++ api/controllers/TimesheetController.js
@@ -59,10 +59,18 @@ module.exports =  {
 
     update: async function(req, res){
         
+        const user_id = req.session.User.id;
+        
         //build new datetime edited by user
         const date = req.body.date;
-        const time = req.body.time;
+        let time = req.body.time;
         const splitedDate = date.split('-')
+
+        //prevent from Mobile Time objects (does not have seconds)
+        if(time.length === 5) {
+            time = time + ':00'
+        }
+
         const splitedTime = time.split(':')
 
         const newDateTime = moment(new Date(parseFloat(splitedDate[0]),     //year
@@ -73,30 +81,40 @@ module.exports =  {
                                             parseFloat(splitedTime[2]),     //seconds
                                             0)).format()
         
+        
+        //fetch list of clocks of the user
+        const userTimesheet = await Timesheet.find( { user_id } )
+        
+        //find current record in the list
+        const index = userTimesheet.findIndex( (x) => x.id == req.params.id );
+        
+        //As the logic of the program does not allow the user to change sorting of records, we can assume that the array will have the record sorted
+        //find previous record
+        const previousTimesheet = userTimesheet[index-1]
+        
+        //find next record
+        const nextTimesheet = userTimesheet[index+1]
+        
         //valid with previous datetime                                            
-        const previousTimesheet = await Timesheet.findOne({id: (Number(req.params.id)-1)})
-
         if (previousTimesheet) {
 
             previousDateTime = moment(new Date(previousTimesheet.clock_in_out_datetime)).format()
             
             if (previousDateTime > newDateTime) {
                 //previous time cannot be higher than new time. In this case, throw error.
-                res.view('pages/timesheet/error', {id: req.params.id, error: 'You are trying to set a lower time than the previous Clock. You must either edit to be a later time than the previous one, or fix the previous times in cascade.'})
+                res.view('pages/timesheet/error', {id: req.params.id, error: 'You are trying to set a lower time than the previous Clock. You must either set a later time than the previous one, or fix the previous times in cascade.'})
                 return;
             }
         }
         
         //valid with next datetime
-        const nextTimesheet = await Timesheet.findOne({id: (Number(req.params.id)+1)})
-
         if (nextTimesheet) {
 
             nextDateTime = moment(new Date(nextTimesheet.clock_in_out_datetime)).format()
             
             if (nextDateTime < newDateTime) {
                 //next time cannot be lower than new time. In this case, throw error.
-                res.view('pages/timesheet/error', {id: req.params.id, error: 'You are trying to set a higher time than the next Clock. You must either edit to be a later time than the next one, or fix the next times in cascade.'})
+                res.view('pages/timesheet/error', {id: req.params.id, error: 'You are trying to set a higher time than the next Clock. You must either set an earlier time than the next one, or fix the next times in cascade.'})
                 return;
             }
         }
